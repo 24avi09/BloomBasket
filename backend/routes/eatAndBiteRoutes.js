27194f14daceead3e5fec977fb235d0eacc2e876
Abhishekh <3avu.yd@gmail.com>
2024-01-01 21:06:49 +0530

    started

---
+++ backend/routes/eatAndBiteRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { getResturentDetails, getAllResturents } = require('../controllers/productController');
+const { isAuthenticatedUser, autherizeRoles } = require('../middleware/auth');
+
+
+
+
+
+
+router.get("/all/resturent", getAllResturents);
+
+router.get("/resturent/:id", getResturentDetails);
+
+
+
+
+
+
+module.exports = router;
